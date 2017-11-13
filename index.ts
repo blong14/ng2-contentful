f2647027802e19a481a20cd268f7b4ab95d44a04
Susan Kern <skern@littlegreensoftware.com>
2017-11-13 11:37:33 -0500

    Added persistent-contentful-request

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export * from './src/services/contentful.service';
 export * from './src/contentful-config';
 export * from './src/contentful-request';
+export * from './src/persistent-contentful-request';
 export * from './src/contentful-types';
